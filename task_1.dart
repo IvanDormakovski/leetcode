b519d9a8beb42e4b24eaab4805572ce5a05ecbf4
Dormakovski <vanya.Dormakovski@yandex.ru>
2022-10-19 23:03:19 +0300

    task 1

---
+++ task_1.dart
@@ -0,0 +1,79 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+ 
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    Map<int, int> map = {};
+    for (var i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return [map[complement] ?? 0, i];
+      }
+      map.putIfAbsent(nums[i], () => i);
+    }
+  return [];
+  }
+}
+
+// class Solution {
+//   List<int> twoSum(List<int> nums, int target) {
+//     for (var i = 0; i < nums.length; i++) {
+//       int position = nums.indexWhere((element) => element == target - nums.elementAt(i), i + 1);
+//       if (position != -1) {
+//         return [i, position];
+//       }
+//     }
+//   return [];
+//   }
+// }
+
+// class Solution {
+//   List<int> twoSum(List<int> nums, int target) {
+//     List<int> ans = [];
+//     for (var i = 0; i < nums.length; i++) {
+//       for (var j = i + 1; j < nums.length; j++) {
+//         if (nums.elementAt(i) + nums.elementAt(j) == target) {
+//           ans..add(i)..add(j);
+//           return ans;
+//         }
+//       }
+//     }
+//   return ans;
+//   }
+// }
+
+
+void main(List<String> args) {
+  Solution solution = Solution();
+  print(solution.twoSum([2,5,5,11], 16));
+}
