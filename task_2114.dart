0f93046926881659ab970244239fb5a4203c6f05
Dormakovski <vanya.Dormakovski@yandex.ru>
2022-10-23 14:31:40 +0300

    task 2114

---
+++ task_2114.dart
@@ -0,0 +1,73 @@
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+// Return the maximum number of words that appear in a single sentence.
+
+// Example 1:
+
+// Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+// Output: 6
+// Explanation:
+// - The first sentence, "alice and bob love leetcode", has 5 words in total.
+// - The second sentence, "i think so too", has 4 words in total.
+// - The third sentence, "this is great thanks very much", has 6 words in total.
+// Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+// Example 2:
+
+// Input: sentences = ["please wait", "continue to fight", "continue to win"]
+// Output: 3
+// Explanation: It is possible that multiple sentences contain the same number of words.
+// In this example, the second and third sentences (underlined) have the same number of words.
+
+// Constraints:
+
+// 1 <= sentences.length <= 100
+// 1 <= sentences[i].length <= 100
+// sentences[i] consists only of lowercase English letters and ' ' only.
+// sentences[i] does not have leading or trailing spaces.
+// All the words in sentences[i] are separated by a single space.
+
+import 'dart:math';
+
+class Solution {
+  int mostWordsFound(List<String> sentences) {
+    int maxLength = 0;
+    for (var sentence in sentences) {
+      maxLength =
+          max(maxLength, RegExp(r'(\s+)').allMatches(sentence).length + 1);
+    }
+    return maxLength;
+  }
+}
+
+// class Solution {
+//   int mostWordsFound(List<String> sentences) {
+//     int maxLength = 0;
+//     RegExp redExp = RegExp(r'(\s+)');
+//     for (var sentence in sentences) {
+//       maxLength =
+//           max(maxLength, redExp.allMatches(sentence).length + 1);
+//     }
+//     return maxLength;
+//   }
+// }
+
+// class Solution {
+//   int mostWordsFound(List<String> sentences) {
+//     int maxLength = 0;
+//     for (var sentence in sentences) {
+//       maxLength = max(maxLength, sentence.split(' ').length);
+//     }
+//     return maxLength;
+//   }
+// }
+
+void main(List<String> args) {
+  Solution solution = Solution();
+  print(solution.mostWordsFound([
+    "alice and bob love leetcode",
+    "i think so too",
+    "this is great thanks very much"
+  ]));
+}
