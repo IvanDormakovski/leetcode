b23ce32728fd41fc461cf6a1fa8956688c17e2c5
Dormakovski <vanya.Dormakovski@yandex.ru>
2022-11-07 23:11:54 +0300

    task 1678

--- task_1678.java
+++ task_1678.java
@@ -25,9 +25,55 @@
 // 1 <= command.length <= 100
 // command consists of "G", "()", and/or "(al)" in some order.
 
+// class Solution1678 {
+//     public String interpret(String command) {
+//         return String.join("al", String.join("o", command.split("\\(\\)")).split("\\(al\\)"));
+//     }
+// }
+
+// class Solution1678 {
+//     public String interpret(String command) {
+//         StringBuilder stringBuilder = new StringBuilder();
+//         for (int i = 0; i < command.length(); i++) {
+//             if (command.charAt(i) == 'G') {
+//                 stringBuilder.append("G");
+//             }
+//             if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+//                 stringBuilder.append("o");
+//                 continue;
+//             }
+//             if (command.charAt(i) == '(' && command.charAt(i + 3) == ')') {
+//                 stringBuilder.append("al");
+//             }
+//         }
+//         return stringBuilder.toString();
+//     }
+// }
+
+// class Solution1678 {
+// public String interpret(String command) {
+// String[] parsed = command.split("");
+// for (int i = 0; i < parsed.length; i++) {
+// if (parsed[i].equals("(") && parsed[i + 1].equals(")")) {
+// parsed[i] = "o";
+// parsed[i + 1] = "";
+// }
+// if (parsed[i].equals("(") && (parsed[i + 1] + parsed[i + 2]).equals("al") &&
+// parsed[i + 3].equals(")")) {
+// parsed[i] = "";
+// parsed[i + 1] = "a";
+// parsed[i + 2] = "l";
+// parsed[i + 3] = "";
+// }
+
+// }
+// return String.join("", parsed);
+// }
+// }
+
 class Solution1678 {
     public String interpret(String command) {
-        String result = command.replaceAll("()", "o").replaceAll("(al)", "al");
+        String result = command.replaceAll("\\(\\)", "o").replaceAll("\\(al\\)","al");
         return result;
     }
 }
@@ -35,7 +81,6 @@ class Solution1678 {
 class Runner {
     public static void main(String[] args) {
         Solution1678 solution = new Solution1678();
-        System.out.print("Test");
         System.out.print(solution.interpret("G()()()()(al)"));
     }
 }
