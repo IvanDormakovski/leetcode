dc12805bd88bfd9783e52c7aac8a9f79310550a6
Dormakovski <vanya.Dormakovski@yandex.ru>
2022-11-15 23:00:13 +0300

    task 938

---
+++ task_938.dart
@@ -0,0 +1,81 @@
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+// Example 1:
+
+// Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+// Output: 32
+// Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+// Example 2:
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+// Output: 23
+// Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 2 * 104].
+// 1 <= Node.val <= 105
+// 1 <= low <= high <= 105
+// All Node.val are unique.
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Solution {
+  int rangeSumBST(TreeNode? root, int low, int high) {
+    if (root == null) return 0; // base case.
+    return (low <= root.val && root.val <= high ? root.val : 0) +
+        rangeSumBST(root.right, low, high) +
+        rangeSumBST(root.left, low, high);
+  }
+}
+
+// class Solution {
+//   int sum = 0;
+//   int rangeSumBST(TreeNode? root, int low, int high) {
+//     if (root == null) return 0; // base case.
+//     if (root.val < low)
+//       return rangeSumBST(root.right, low, high); // left branch excluded.
+//     if (root.val > high)
+//       return rangeSumBST(root.left, low, high); // right branch excluded.
+//     return root.val +
+//         rangeSumBST(root.right, low, high) +
+//         rangeSumBST(root.left, low, high); // count in both children.
+//   }
+// }
+
+// class Solution {
+//   int sum = 0;
+//   int rangeSumBST(TreeNode? root, int low, int high) {
+//     if (root == null) return sum;
+//     if (root.val >= low && root.val <= high) sum += root.val;
+//     if (root.left != null) rangeSumBST(root.left, low, high);
+//     if (root.right != null) rangeSumBST(root.right, low, high);
+//     return sum;
+//   }
+// }
+
+void main(List<String> args) {
+  Solution solution = Solution();
+  print(solution.rangeSumBST(
+    TreeNode(
+      10,
+      TreeNode(
+        5,
+        TreeNode(3),
+        TreeNode(7),
+      ),
+      TreeNode(
+        15,
+        null,
+        TreeNode(18),
+      ),
+    ),
+    7,
+    15,
+  ));
+}
